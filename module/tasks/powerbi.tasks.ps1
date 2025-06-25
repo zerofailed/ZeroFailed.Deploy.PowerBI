d2ea6265dc0da2e9fa71b76f31338d4537e49634
Jessica Hill <Jessica.Hill@endjin.com>
2025-06-25 12:13:44 +0100

    WIP updating task

--- module/tasks/powerbi.tasks.ps1
+++ module/tasks/powerbi.tasks.ps1
@@ -2,18 +2,18 @@
 # Copyright (c) Endjin Limited. All rights reserved.
 # </copyright>
 
-task deployPowerBISharedCloudConnection {
+task deployPowerBISharedCloudConnection -After ProvisionCore {
 
     $token = Get-AzAccessToken -AsSecureString -ResourceUrl 'https://api.fabric.microsoft.com'
 
     foreach ($connection in $cloudConnection) {    
 
         $splat = @{
-            DisplayName = $connection.displayName
+            DisplayName = {$connection.displayName}
             ConnectionType = $connection.connectionType
-            Parameters = $connection.parameters
+            Parameters = $connection.parameters | Resolve-Value
             ServicePrincipalClientId = $connection.servicePrincipalClientId
-            ServicePrincipalSecret = $connection.servicePrincipalSecret
+            ServicePrincipalSecret = $connection.servicePrincipalSecret | Resolve-Value
             TenantId = $connection.tenantId
             AccessToken = $token.Token
         }
